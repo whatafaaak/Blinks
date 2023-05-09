5c7974b4c970da852633749046703537f12b187f
whatafaaak <133053660+whatafaaak@users.noreply.github.com>
2023-05-09 22:35:21 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import TextBlock from './textBlock';
+import { ImageSlide } from './components/ImageSlide';
+import Lore from './components/Lore'
+import Gaming from './components/Gaming';
+import Features from './components/Features';
+import Team from './components/Team';
+import Footer from './components/Footer'
+import './assets/fonts/Just-Cosmic.otf';
+
+function App() {
+  
+  return (
+    <>
+    
+    <Navbar/>
+    
+    <Dashboard/>
+    
+    <ImageSlide/>
+    
+    
+    <Lore/>   
+          
+    <Gaming/>   
+    <Features/>  
+    
+    <Team/>
+    
+    <Footer/>
+    
+        
+    
+    </>
+
+  );
+}
+
+export default App;
